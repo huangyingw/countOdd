9ea0c8002362275f7f373068f469abe7c6188aee
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-12 22:03:28 +0800

    n

--- src/countOdd.java
+++ src/countOdd.java
@@ -2,7 +2,14 @@ import java.util.ArrayList;
 
 public class countOdd {
 	public static ArrayList<String> fn(int num) {
+		ArrayList<String> result = new ArrayList<String>();
+		if (num == 0) {
+			result.add("0");
+			return result;
+		}
+		for (String nav : fn(num - 1)) {
 
+		}
 		return null;
 	}
 
